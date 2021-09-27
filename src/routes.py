a885ebfb1e70c5295565afa4da12bf70dd6ed7ca
briancastro-bc <camilitooso77@gmail.com>
2021-09-27 17:02:04 -0500

    Desarrollo: finalizando autenticación de usuarios. BC V-0.5.1

--- src/routes.py
+++ src/routes.py
@@ -1,7 +1,7 @@
-from src.controllers import IndexController, SignupController
+from src.controllers import IndexController, SignupController, SigninController
 
 routes = {
     "index": "/", "index_controller": IndexController.as_view("index"),
     "signup": "/signup", "signup_controller": SignupController.as_view("signup"),
-    "signin": "/signin", "signin_controller": SignupController.as_view("signin")
+    "signin": "/signin", "signin_controller": SigninController.as_view("signin")
 }
